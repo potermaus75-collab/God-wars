1d72a594e17c9ec5b724ac4c426d72a814b899cc
potermaus75-collab <potermaus75@gmail.com>
2026-02-11 20:39:21 +0900

    Create data_recipes.js

---
+++ data_recipes.js
@@ -0,0 +1,18 @@
+const RECIPES = [
+    // [그리스]
+    { id: "rcp_01", result: "g_gr_u1", mat1: "g_gr_c1", mat2: "mat_g_01", cost: 500, chance: 100 }, 
+    { id: "rcp_02", result: "g_gr_r1", mat1: "g_gr_u1", mat2: "mat_g_03", cost: 2000, chance: 90 }, 
+    { id: "rcp_03", result: "g_gr_e1", mat1: "g_gr_r1", mat2: "mat_g_rare", cost: 10000, chance: 50 }, 
+    
+    // [한국]
+    { id: "rcp_04", result: "g_kr_r2", mat1: "g_kr_c1", mat2: "mat_k_02", cost: 3000, chance: 80 }, 
+    { id: "rcp_05", result: "g_kr_g1", mat1: "g_kr_e2", mat2: "mat_k_rare", cost: 50000, chance: 30 }, 
+
+    // [북유럽]
+    { id: "rcp_06", result: "g_nr_r1", mat1: "g_nr_c1", mat2: "mat_n_02", cost: 2500, chance: 85 }, 
+    { id: "rcp_07", result: "g_nr_g1", mat1: "g_nr_l1", mat2: "mat_n_rare", cost: 60000, chance: 25 }, 
+
+    // [이집트]
+    { id: "rcp_08", result: "g_eg_u1", mat1: "g_eg_c1", mat2: "mat_e_03", cost: 1000, chance: 95 }, 
+    { id: "rcp_09", result: "g_eg_g1", mat1: "g_eg_l1", mat2: "mat_e_rare", cost: 70000, chance: 20 } 
+];
