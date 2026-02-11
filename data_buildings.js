5c44f2c1910776101eff55a54050a7eddec48d4f
potermaus75-collab <potermaus75@gmail.com>
2026-02-11 20:39:01 +0900

    Create data_buildings.js

---
+++ data_buildings.js
@@ -0,0 +1,7 @@
+const BUILDINGS = [
+    { id: "b_01", name: "소작농장", base_cost: 500, income: 50, desc: "작은 농장입니다. (시간당 +50 G)" },
+    { id: "b_02", name: "은 광산", base_cost: 2500, income: 300, desc: "은이 나오는 광산. (시간당 +300 G)" },
+    { id: "b_03", name: "무역 항구", base_cost: 12000, income: 1500, desc: "배가 드나드는 항구. (시간당 +1,500 G)" },
+    { id: "b_04", name: "파르테논 신전", base_cost: 60000, income: 8000, desc: "많은 헌금이 들어옵니다. (시간당 +8,000 G)" },
+    { id: "b_05", name: "황금 왕궁", base_cost: 300000, income: 50000, desc: "권력의 중심. (시간당 +50,000 G)" }
+];
